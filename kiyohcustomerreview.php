6f3b9c77802fa86a9384f84387e07f63c0d0f18a
Aleksandr Drobot <a.drobot@codetiburon.com>
2015-01-12 11:10:27 +0200

    KIYOH-34 change license

--- kiyohcustomerreview.php
+++ kiyohcustomerreview.php
@@ -1,20 +1,29 @@
 <?php
-
 /**
- * 2014 Interactivated.me
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php
- *
- *  @author    Interactivated <contact@interactivated.me>
- *  @copyright 2014 Interactivated.me
- *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
- *
- * */
+* 2014-2015 Interactivated.me
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    Interactivated <contact@interactivated.me>
+*  @copyright 2014-2015 Interactivated.me
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
 if (!defined('_PS_VERSION_'))
     exit;
 
